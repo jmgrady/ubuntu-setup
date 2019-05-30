7ae39ec4408ad35d510f15c234205177941bd2a4
Jim Grady <jimgrady.jg@gmail.com>
2019-05-30 12:02:58 -0500

    Remove need for separate script to install ansible dependencies

--- README.md
+++ README.md
@@ -4,8 +4,15 @@
 
 ## Roles ##
 
+  **ansible-depends** - installs the packages required to run subsequent Ansible
+  modules
+
+  **headless** - sets configuration options that make sense when the device will be used as a headless node, such as, configuring the power button to shutdown when there is no display to confirm or to choose between Shutdown, Restart and Cancel.  This is intended for a Server installation.
+
+  **mediaplayer** - installs various media tools, such as spotify and clementine.  This role requires a Desktop installation.
+
+  **setup_apache** - installs the apache2 web server
+
+  **setup_mongodb** - installs the MongoDB database (from mongodb.org, //not// the Ubuntu package) and installs it as a service
+
   **wifi_ap** - sets up the wifi interface as a wifi access point (hotspot)
-
-  **headless** - sets configuration options that make sense when the device will be used as a headless node, such as, configuring the power button to shutdown when there is no display to confirm or to choose between Shutdown, Restart and Cancel.
-
-  **mediaplayer** - installs various media tools, such as spotify and clementine
--- setup-target.sh
+++ setup-target.sh
@@ -5,9 +5,7 @@
 # setup-target.sh
 #
 # setup-target.sh will initialize an Ubuntu machine so that it can be used
-# as an Ubuntu SERVER for the Rapid Language Collection tool.  It will install
-# and run a simple script to prep the target for ansible scripts that will be
-# run to complete the setup.
+# as backend for the Rapid Language Collection tool.
 #
 # Requirements
 #  - target
@@ -26,31 +24,66 @@
 #    Desktop or Server respectively.
 #
 
-if [ $# -ne 1 ] ; then
-  echo "Usage: $0 user@machinename"
-else
-  TARGET=$1;
+function print_usage()
+{
+  echo "Usage: $0 [-c|--copyid|-h|--help|-t] user@machinename"
+  echo "where:"
+  echo "   -c"
+  echo "   --copyid   will use ssh-copy-id to copy your id to user@machinename"
+  echo "              so that you do not need to enter your password to connect"
+  echo "   -h"
+  echo "   --help     print this message"
+  echo "   -t         test argument parsing of the script - does not setup the"
+  echo "              target"
+
+}
+
+COPYID=0
+JUST_TEST=0
+while [ "$#" -gt 0 ]; do
+  case $1 in
+    "-c" | "--copyid")
+        COPYID=1
+        ;;
+    "-h" | "--help")
+        print_usage
+        exit 0
+        ;;
+    "-t")
+        JUST_TEST=1
+        ;;
+    -*)
+        echo "option $1 not recognized - ignored"
+        ;;
+    *)
+        TARGET=$1;
+        ;;
+  esac
+  shift
+done
+
+if [[ -n "$TARGET" ]] ; then
   USER=`expr $TARGET : '\([^@]*\)@.*'`
   MACHINE=`expr $TARGET : '.*@\([^@]*\)$'`
-  if [[ ! ( -n "$USER" && -n "$MACHINE" ) ]] ; then
-    echo "Usage: $0 user@machinename"
-    exit 1
-  fi
-  #echo "USER == $USER"
-  #echo "MACHINE == $MACHINE"
-  #exit 0
-  echo "Setting up $TARGET"
-  # Copy the actual setup script to the target machine
-  rsync -avz prep-target.sh $TARGET:/tmp
-  ssh $TARGET chmod +x /tmp/prep-target.sh
-
-  echo "*************************************************************"
-  echo -e "\nOpening a secure shell connection to $TARGET. Run:"
-  echo -e "\n\t/tmp/prep-target.sh"
-  echo -e "\n\tExit the secure shell when complete."
-  echo "*************************************************************"
-  ssh $TARGET
-  ssh $TARGET rm /tmp/prep-target.sh
-
-  ansible-playbook -i hosts playbook_setup.yml --limit $MACHINE -u $USER -K
 fi
+
+if [[ ! ( -n "$USER" && -n "$MACHINE" ) ]] ; then
+  print_usage
+  exit 1
+fi
+
+if [ "$JUST_TEST" -eq 1 ] ; then
+  echo "USER == $USER"
+  echo "MACHINE == $MACHINE"
+  echo "COPYID == $COPYID"
+  exit 0
+fi
+
+echo "Setting up $TARGET"
+
+if [ "$COPYID" -eq 1 ] ; then
+  echo "Copying ssh id to $USER@$MACHINE"
+  ssh-copy-id "$USER@$MACHINE"
+fi
+
+ansible-playbook -i hosts playbook_setup.yml --limit $MACHINE -u $USER -K
